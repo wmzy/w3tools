fca51d293aeeba9dd41d634a780f959906728562
wmzy <1256573276@qq.com>
2025-04-28 10:12:53 +0800

    feat: enhance client creation with dynamic transport selection based on RPC URLs

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,4 +1,4 @@
-import { createPublicClient, http } from 'viem';
+import { createPublicClient, fallback, http, Transport } from 'viem';
 import { GlobalOptions } from './types';
 import { getChain } from './config';
 import { globalConfig } from './config';
@@ -7,9 +7,21 @@ export function createClient(options: GlobalOptions) {
   const chain = getChain(
     options.chain || globalConfig.get('currentChain') || 'mainnet'
   );
+  if (!chain) {
+    throw new Error(`Chain ${options.chain} not found`);
+  }
+  const rpcUrls = globalConfig.get('rpcUrls')[chain.id];
+  let transport: Transport = http();
+  if (rpcUrls?.length) {
+    if (rpcUrls.length === 1) {
+      transport = http(rpcUrls[0]);
+    } else {
+      transport = fallback(rpcUrls.map((r) => http(r)));
+    }
+  }
   return createPublicClient({
     chain,
-    transport: http(),
+    transport,
     batch: {
       multicall: true,
     },
